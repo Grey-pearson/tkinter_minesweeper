d05e0f79d01d6b1ad830b4040939618057c510a7
grey <greytpearson@gmail.com>
2024-06-18 16:52:49 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,7 @@
-import ttkbootstrap as ttk
-from ttkbootstrap.constants import *
-
-class Mine_sweeper(ttk.Tk):
-    def __init__(self):
-        self.board_size = 10
-        self.bombs_amount = 13
-        self.root = ttk.Window()
-
-    def intro(self):
-        pass
-
-    def create_board(self):
-        pass
+import pygame, sys
+from random import randrange
+from pygame.locals import * 
+from ctypes import c_int, WINFUNCTYPE, windll
+from ctypes.wintypes import HWND, LPCSTR, UINT
+def MessageBox(title, text, style):
+    return windll.user32.MessageBoxW(0, text, title, style)
