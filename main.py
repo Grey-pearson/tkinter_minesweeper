6cab221daab8a63c5b257cd09848a606fdf9f792
grey <greytpearson@gmail.com>
2024-06-19 14:51:17 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,14 @@
 import pygame, sys
 from random import randrange
 from pygame.locals import *
+from settings import *
 
 pygame.init()
 fps = pygame.time.Clock()
-bomb = pygame.image.load("imgs/bomb.png")
+
 pygame.display.set_icon(bomb)
-surfObj = pygame.display.set_mode((173, 200))
-pygame.display.set_caption("Minesweeper")
+surfObj = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption(TITLE)
 
-
-
-
-class Block:
-    def __init__(self, type) -> None:
-        self.type = type
-        self.img = 
+class Map:
+    
